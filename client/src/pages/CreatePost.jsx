87d19f586982cf1840a929580b9c30e118a0d426
Sanula Kariyapperuma <sanula2021@gmail.com>
2024-07-16 02:19:26 +0530

    new

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -19,6 +19,18 @@ const CreatePost = () => {
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
+  const handleSubmit = ()=>{
+
+  }
+
+  const handleChange = (e) => {
+    
+  }
+
+  const handleSurpriseMe = () => {
+    
+  }
+
   return (
     <section className='max-w-7xl mx-auto'>
       <div>
@@ -33,6 +45,31 @@ const CreatePost = () => {
             stunning image through DALL-E AI and share them with the community
           </p>
         </div>
+
+        <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
+          <div className='flex flex-col gap-5'>
+<FormField
+labelName="Your name"
+type="text"
+name="name"
+placeholder="John Doe"
+value={form.name}
+handleChange={handleChange}
+/>
+<FormField
+labelName="promt"
+type="text"
+name="promt"
+placeholder="a painting of a fox in the style of Starry Night"
+value={form.promt}
+handleChange={handleChange}
+isSurpriseMe
+handleSurpriseMe={handleSurpriseMe}
+/>
+          </div>
+        </form>
+
+
     </section>
   )
 }
