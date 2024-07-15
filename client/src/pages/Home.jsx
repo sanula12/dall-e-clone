39cb3e2a965313529033cb564019bd971373e723
Sanula Kariyapperuma <sanula2021@gmail.com>
2024-07-16 01:51:34 +0530

    up

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,7 +4,16 @@ import { Loader, Card,FormField } from '../components'
 
 const RenderCards = ({ data, title }) => {
   
-  
+  if (data?.lenght> 0){
+    return data.map((post) => <Card key={post._id} {...post}/>)
+  }
+
+  return (
+    <h2 className='mt-5 font-bold text-[#6469ff]
+    text-xl uppercase '>
+{title}
+    </h2>
+  )
 }
 const Home = () => {
 
@@ -48,6 +57,19 @@ Showing results for <span className='text-[#222328]'>{searchText}</span>
       <div className='grid lg:grid-cols-4 sm:grid-cols-3
       xs:grid-cols-2 grid-cols-1 gap-3'>
 
+{searchText ?(
+  <RenderCards 
+  data={[]}
+  title="No search results found"
+/>
+) : (
+  <RenderCards
+  data={[]}
+  title="No posts found"
+  />
+)
+
+}
       </div>
       </>
     )}
